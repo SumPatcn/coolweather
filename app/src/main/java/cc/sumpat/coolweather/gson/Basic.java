7f808212ff095c1dc8f5f0959ff695b84e6472d2
sum <pat565178018@gmail.com>
2019-02-01 16:42:52 +0800

    update GsonObject

---
+++ app/src/main/java/cc/sumpat/coolweather/gson/Basic.java
@@ -0,0 +1,88 @@
+package cc.sumpat.coolweather.gson;
+
+public class Basic {
+
+    /**
+     * location	地区／城市名称
+     * cid	地区／城市ID
+     * lat	地区／城市纬度
+     * lon	地区／城市经度
+     * parent_city	该地区／城市的上级城市
+     * admin_area	该地区／城市所属行政区域
+     * cnty	该地区／城市所属国家名称
+     * tz	该地区／城市所在时区
+     */
+
+    private String cid;
+    private String location;
+    private String parent_city;
+    private String admin_area;
+    private String cnty;
+    private String lat;
+    private String lon;
+    private String tz;
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getParent_city() {
+        return parent_city;
+    }
+
+    public void setParent_city(String parent_city) {
+        this.parent_city = parent_city;
+    }
+
+    public String getAdmin_area() {
+        return admin_area;
+    }
+
+    public void setAdmin_area(String admin_area) {
+        this.admin_area = admin_area;
+    }
+
+    public String getCnty() {
+        return cnty;
+    }
+
+    public void setCnty(String cnty) {
+        this.cnty = cnty;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getTz() {
+        return tz;
+    }
+
+    public void setTz(String tz) {
+        this.tz = tz;
+    }
+}
---
+++ app/src/main/java/cc/sumpat/coolweather/gson/Weather.java
@@ -0,0 +1,13 @@
+package cc.sumpat.coolweather.gson;
+
+public class Weather {
+    public String status;
+
+    public Basic basic;
+
+    public Now now;
+
+    public LifeStyle lifeStyle;
+
+    public DailyForecast dailyForecast;
+}
