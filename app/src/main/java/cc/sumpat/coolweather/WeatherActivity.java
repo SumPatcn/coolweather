6d76520307ebe9506c83b7b2b3c08da907ec3aac
sum <pat565178018@gmail.com>
2019-02-15 16:59:51 +0800

    test 0.0.1

--- app/src/main/java/cc/sumpat/coolweather/WeatherActivity.java
+++ app/src/main/java/cc/sumpat/coolweather/WeatherActivity.java
@@ -13,7 +13,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.List;
 
+import cc.sumpat.coolweather.gson.Aqi;
 import cc.sumpat.coolweather.gson.DailyForecast;
 import cc.sumpat.coolweather.gson.LifeStyle;
 import cc.sumpat.coolweather.gson.Weather;
@@ -63,27 +65,31 @@ public class WeatherActivity extends AppCompatActivity {
         sportText = findViewById(R.id.sport_text);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences
                 (this);
-        String weatherString = preferences.getString("weather", "id");
+        String weatherString = preferences.getString("weather", null);
         if (weatherString != null) {
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            Aqi aqi = Utility.handleAqiResponse(weatherString);
             showWeatherInfo(weather);
+            showAqiInfo(aqi);
         } else {
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
+            requestAqi(weatherId);
         }
     }
 
     public void requestWeather(final String weatherId) {
-        String weatherUrl = "https://free-api.heweather.net/s6/weather?location" + weatherId +
+        String weatherUrl = "https://free-api.heweather.net/s6/weather?location=" + weatherId +
                 "&key=2e34dcfa8cf2445c966410994b69f060";
+
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(WeatherActivity.this, "获取信息失败",
+                        Toast.makeText(WeatherActivity.this, "服务器获取信息失败2",
                                 Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -104,7 +110,52 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.apply();
                             showWeatherInfo(weather);
                         } else {
-                            Toast.makeText(WeatherActivity.this, "获取信息失败",
+                            Toast.makeText(WeatherActivity.this, "服务器获取信息失败",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    public void requestAqi(final String weatherId) {
+        StringBuilder temp = new StringBuilder(weatherId);
+        temp.deleteCharAt(temp.length() - 1);
+        temp.deleteCharAt(temp.length() - 1);
+        temp.append("01");
+
+        String aqiUrl = "https://free-api.heweather.net/s6/air/now?location=" + temp.toString() +
+                "&key=2e34dcfa8cf2445c966410994b69f060";
+
+        HttpUtil.sendOkHttpRequest(aqiUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(WeatherActivity.this, "服务器获取信息失败2",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Aqi aqi = Utility.handleAqiResponse(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (aqi != null && "ok".equals(aqi.getStatus())) {
+                            SharedPreferences.Editor editor = PreferenceManager.
+                                    getDefaultSharedPreferences(WeatherActivity.this)
+                                    .edit();
+                            editor.putString("aqi", responseText);
+                            editor.apply();
+                            showAqiInfo(aqi);
+                        } else {
+                            Toast.makeText(WeatherActivity.this, "服务器获取信息失败",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -123,19 +174,47 @@ public class WeatherActivity extends AppCompatActivity {
         degreeText.setText(degree);
         weatherInfoText.setText(weatherInfo);
         forecastLayout.removeAllViews();
+        for (LifeStyle lifeStyle : weather.lifeStyleList) {
+            if (lifeStyle.getType().equals("comf")) {
+                String comfort = "舒适度：" + lifeStyle.getTxt();
+                comfortText.setText(comfort);
+            } else {
+                comfortText.setText("获取指数信息失败");
+            }
+            if (lifeStyle.getType().equals("cw")) {
+                String carWash = "洗车指数：" + lifeStyle.getTxt();
+                carWashText.setText(carWash);
+            } else {
+                carWashText.setText("获取指数信息失败");
+            }
+            if (lifeStyle.getType().equals("sport")) {
+                String sport = "运动指数：" + lifeStyle.getTxt();
+                sportText.setText(sport);
+            } else {
+                sportText.setText("获取指数信息失败");
+            }
+        }
         for (DailyForecast dailyForecast : weather.dailyForecastList) {
             View view = LayoutInflater.from(this).inflate(R.layout.forecast_item,
                     forecastLayout, false);
-            TextView dateText=view.findViewById(R.id.date_text);
-            TextView infoText=view.findViewById(R.id.info_text);
-            TextView maxText=view.findViewById(R.id.max_text);
-            TextView minText=view.findViewById(R.id.min_text);
+            TextView dateText = view.findViewById(R.id.date_text);
+            TextView infoText = view.findViewById(R.id.info_text);
+            TextView maxText = view.findViewById(R.id.max_text);
+            TextView minText = view.findViewById(R.id.min_text);
             dateText.setText(dailyForecast.getDate());
             infoText.setText(dailyForecast.getCond_txt_d());
             maxText.setText(dailyForecast.getTmp_max());
             minText.setText(dailyForecast.getTmp_min());
             forecastLayout.addView(view);
         }
-        //
+    }
+
+    private void showAqiInfo(Aqi aqi) {
+        if (aqi != null) {
+            aqiText.setText(aqi.getAir_now_city().getAqi());
+            pm25Text.setText(aqi.getAir_now_city().getPm25());
+        } else {
+            Toast.makeText(this, "获取空气质量信息失败", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/cc/sumpat/coolweather/gson/Aqi.java
+++ app/src/main/java/cc/sumpat/coolweather/gson/Aqi.java
@@ -1,21 +1,46 @@
 package cc.sumpat.coolweather.gson;
 
+import java.util.List;
+
 public class Aqi {
 
     /**
-     * pub_time	数据发布时间,格式yyyy-MM-dd HH:mm
-     * aqi	空气质量指数，AQI和PM25的关系
-     * main	主要污染物
-     * qlty	空气质量，取值范围:优，良，轻度污染，中度污染，重度污染，严重污染，查看计算方式
-     * pm10	pm10
-     * pm25	pm25
-     * no2	二氧化氮
-     * so2	二氧化硫
-     * co	一氧化碳
-     * o3	臭氧
+     * basic : {"cid":"CN101260101","location":"贵阳","parent_city":"贵阳","admin_area":"贵州","cnty":"中国","lat":"26.57834244","lon":"106.71347809","tz":"+8.00"}
+     * update : {"loc":"2019-02-15 10:56","utc":"2019-02-15 02:56"}
+     * status : ok
+     * air_now_city : {"aqi":"23","qlty":"优","main":"-","pm25":"12","pm10":"23","no2":"17","so2":"10","co":"0.8","o3":"16","pub_time":"2019-02-15 10:00"}
+     * air_now_station : [{"air_sta":"马鞍山","aqi":"13","asid":"CNA1439","co":"0.4","lat":"26.6029","lon":"106.6856","main":"-","no2":"14","o3":"13","pm10":"13","pm25":"4","pub_time":"2019-02-15 10:00","qlty":"优","so2":"6"},{"air_sta":"市环保站","aqi":"25","asid":"CNA1440","co":"0.9","lat":"26.5689","lon":"106.6971","main":"-","no2":"23","o3":"13","pm10":"25","pm25":"7","pub_time":"2019-02-15 10:00","qlty":"优","so2":"8"},{"air_sta":"鉴湖路","aqi":"58","asid":"CNA1441","co":"0.6","lat":"26.6266","lon":"106.6243","main":"PM2.5","no2":"12","o3":"10","pm10":"55","pm25":"41","pub_time":"2019-02-15 10:00","qlty":"良","so2":"3"},{"air_sta":"燕子冲","aqi":"23","asid":"CNA1442","co":"0.8","lat":"26.6343","lon":"106.7487","main":"-","no2":"11","o3":"21","pm10":"23","pm25":"14","pub_time":"2019-02-15 10:00","qlty":"优","so2":"6"},{"air_sta":"碧云窝","aqi":"19","asid":"CNA1443","co":"0.8","lat":"26.4364","lon":"106.6554","main":"-","no2":"17","o3":"15","pm10":"19","pm25":"3","pub_time":"2019-02-15 10:00","qlty":"优","so2":"8"},{"air_sta":"中院村","aqi":"16","asid":"CNA1444","co":"0.9","lat":"26.5155","lon":"106.6948","main":"-","no2":"18","o3":"18","pm10":"13","pm25":"11","pub_time":"2019-02-15 10:00","qlty":"优","so2":"22"},{"air_sta":"红边门","aqi":"13","asid":"CNA1445","co":"0.9","lat":"26.6009","lon":"106.7105","main":"-","no2":"20","o3":"10","pm10":"13","pm25":"5","pub_time":"2019-02-15 10:00","qlty":"优","so2":"18"},{"air_sta":"新华路","aqi":"16","asid":"CNA1446","co":"0.8","lat":"26.5697","lon":"106.7164","main":"-","no2":"16","o3":"17","pm10":"16","pm25":"6","pub_time":"2019-02-15 10:00","qlty":"优","so2":"4"},{"air_sta":"太慈桥","aqi":"25","asid":"CNA1447","co":"0.9","lat":"26.5495","lon":"106.6867","main":"-","no2":"28","o3":"8","pm10":"25","pm25":"3","pub_time":"2019-02-15 10:00","qlty":"优","so2":"12"},{"air_sta":"桐木岭","aqi":"28","asid":"CNA1552","co":"0.7","lat":"26.3003","lon":"106.805","main":"-","no2":"5","o3":"33","pm10":"0","pm25":"19","pub_time":"2019-02-15 10:00","qlty":"优","so2":"4"}]
      */
 
+    private BasicBean basic;
+    private UpdateBean update;
+    private String status;
     private AirNowCityBean air_now_city;
+    private List<AirNowStationBean> air_now_station;
+
+    public BasicBean getBasic() {
+        return basic;
+    }
+
+    public void setBasic(BasicBean basic) {
+        this.basic = basic;
+    }
+
+    public UpdateBean getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(UpdateBean update) {
+        this.update = update;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     public AirNowCityBean getAir_now_city() {
         return air_now_city;
@@ -25,18 +50,138 @@ public class Aqi {
         this.air_now_city = air_now_city;
     }
 
+    public List<AirNowStationBean> getAir_now_station() {
+        return air_now_station;
+    }
+
+    public void setAir_now_station(List<AirNowStationBean> air_now_station) {
+        this.air_now_station = air_now_station;
+    }
+
+    public static class BasicBean {
+        /**
+         * cid : CN101260101
+         * location : 贵阳
+         * parent_city : 贵阳
+         * admin_area : 贵州
+         * cnty : 中国
+         * lat : 26.57834244
+         * lon : 106.71347809
+         * tz : +8.00
+         */
+
+        private String cid;
+        private String location;
+        private String parent_city;
+        private String admin_area;
+        private String cnty;
+        private String lat;
+        private String lon;
+        private String tz;
+
+        public String getCid() {
+            return cid;
+        }
+
+        public void setCid(String cid) {
+            this.cid = cid;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public String getParent_city() {
+            return parent_city;
+        }
+
+        public void setParent_city(String parent_city) {
+            this.parent_city = parent_city;
+        }
+
+        public String getAdmin_area() {
+            return admin_area;
+        }
+
+        public void setAdmin_area(String admin_area) {
+            this.admin_area = admin_area;
+        }
+
+        public String getCnty() {
+            return cnty;
+        }
+
+        public void setCnty(String cnty) {
+            this.cnty = cnty;
+        }
+
+        public String getLat() {
+            return lat;
+        }
+
+        public void setLat(String lat) {
+            this.lat = lat;
+        }
+
+        public String getLon() {
+            return lon;
+        }
+
+        public void setLon(String lon) {
+            this.lon = lon;
+        }
+
+        public String getTz() {
+            return tz;
+        }
+
+        public void setTz(String tz) {
+            this.tz = tz;
+        }
+    }
+
+    public static class UpdateBean {
+        /**
+         * loc : 2019-02-15 10:56
+         * utc : 2019-02-15 02:56
+         */
+
+        private String loc;
+        private String utc;
+
+        public String getLoc() {
+            return loc;
+        }
+
+        public void setLoc(String loc) {
+            this.loc = loc;
+        }
+
+        public String getUtc() {
+            return utc;
+        }
+
+        public void setUtc(String utc) {
+            this.utc = utc;
+        }
+    }
+
     public static class AirNowCityBean {
         /**
-         * aqi : 24
+         * aqi : 23
          * qlty : 优
          * main : -
-         * pm25 : 14
-         * pm10 : 24
-         * no2 : 14
-         * so2 : 16
+         * pm25 : 12
+         * pm10 : 23
+         * no2 : 17
+         * so2 : 10
          * co : 0.8
-         * o3 : 33
-         * pub_time : 2019-02-11 14:00
+         * o3 : 16
+         * pub_time : 2019-02-15 10:00
          */
 
         private String aqi;
@@ -128,6 +273,152 @@ public class Aqi {
 
         public void setPub_time(String pub_time) {
             this.pub_time = pub_time;
+        }
+    }
+
+    public static class AirNowStationBean {
+        /**
+         * air_sta : 马鞍山
+         * aqi : 13
+         * asid : CNA1439
+         * co : 0.4
+         * lat : 26.6029
+         * lon : 106.6856
+         * main : -
+         * no2 : 14
+         * o3 : 13
+         * pm10 : 13
+         * pm25 : 4
+         * pub_time : 2019-02-15 10:00
+         * qlty : 优
+         * so2 : 6
+         */
+
+        private String air_sta;
+        private String aqi;
+        private String asid;
+        private String co;
+        private String lat;
+        private String lon;
+        private String main;
+        private String no2;
+        private String o3;
+        private String pm10;
+        private String pm25;
+        private String pub_time;
+        private String qlty;
+        private String so2;
+
+        public String getAir_sta() {
+            return air_sta;
+        }
+
+        public void setAir_sta(String air_sta) {
+            this.air_sta = air_sta;
+        }
+
+        public String getAqi() {
+            return aqi;
+        }
+
+        public void setAqi(String aqi) {
+            this.aqi = aqi;
+        }
+
+        public String getAsid() {
+            return asid;
+        }
+
+        public void setAsid(String asid) {
+            this.asid = asid;
+        }
+
+        public String getCo() {
+            return co;
+        }
+
+        public void setCo(String co) {
+            this.co = co;
+        }
+
+        public String getLat() {
+            return lat;
+        }
+
+        public void setLat(String lat) {
+            this.lat = lat;
+        }
+
+        public String getLon() {
+            return lon;
+        }
+
+        public void setLon(String lon) {
+            this.lon = lon;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public void setMain(String main) {
+            this.main = main;
+        }
+
+        public String getNo2() {
+            return no2;
+        }
+
+        public void setNo2(String no2) {
+            this.no2 = no2;
+        }
+
+        public String getO3() {
+            return o3;
+        }
+
+        public void setO3(String o3) {
+            this.o3 = o3;
+        }
+
+        public String getPm10() {
+            return pm10;
+        }
+
+        public void setPm10(String pm10) {
+            this.pm10 = pm10;
+        }
+
+        public String getPm25() {
+            return pm25;
+        }
+
+        public void setPm25(String pm25) {
+            this.pm25 = pm25;
+        }
+
+        public String getPub_time() {
+            return pub_time;
+        }
+
+        public void setPub_time(String pub_time) {
+            this.pub_time = pub_time;
+        }
+
+        public String getQlty() {
+            return qlty;
+        }
+
+        public void setQlty(String qlty) {
+            this.qlty = qlty;
+        }
+
+        public String getSo2() {
+            return so2;
+        }
+
+        public void setSo2(String so2) {
+            this.so2 = so2;
         }
     }
 }
