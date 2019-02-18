2bdede6be75587d8d4b91a85dd7dc5c5ad2150c6
sum <pat565178018@gmail.com>
2019-02-18 16:58:30 +0800

    debug: aqi handle

--- app/src/main/java/cc/sumpat/coolweather/WeatherActivity.java
+++ app/src/main/java/cc/sumpat/coolweather/WeatherActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -13,12 +14,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.util.List;
 
 import cc.sumpat.coolweather.gson.Aqi;
 import cc.sumpat.coolweather.gson.DailyForecast;
 import cc.sumpat.coolweather.gson.LifeStyle;
 import cc.sumpat.coolweather.gson.Weather;
+import cc.sumpat.coolweather.gson.Aqi;
 import cc.sumpat.coolweather.util.HttpUtil;
 import cc.sumpat.coolweather.util.Utility;
 import okhttp3.Call;
@@ -26,6 +27,8 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+    private String TAG = "WeatherActivity";
+
     private ScrollView weatherLayout;
 
     private TextView titleCity;
@@ -38,9 +41,7 @@ public class WeatherActivity extends AppCompatActivity {
 
     private LinearLayout forecastLayout;
 
-    private TextView aqiText;
-
-    private TextView pm25Text;
+    private LinearLayout aqiLayout;
 
     private TextView comfortText;
 
@@ -58,19 +59,17 @@ public class WeatherActivity extends AppCompatActivity {
         degreeText = findViewById(R.id.degree_text);
         weatherInfoText = findViewById(R.id.weather_info_text);
         forecastLayout = findViewById(R.id.forecast_layout);
-        aqiText = findViewById(R.id.aqi_text);
-        pm25Text = findViewById(R.id.pm25_text);
+        aqiLayout =findViewById(R.id.aqi_layout);
         comfortText = findViewById(R.id.comf_text);
         carWashText = findViewById(R.id.cw_text);
         sportText = findViewById(R.id.sport_text);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences
                 (this);
         String weatherString = preferences.getString("weather", null);
+        Log.d(TAG, "onCreate: " + weatherString);
         if (weatherString != null) {
             Weather weather = Utility.handleWeatherResponse(weatherString);
-            Aqi aqi = Utility.handleAqiResponse(weatherString);
             showWeatherInfo(weather);
-            showAqiInfo(aqi);
         } else {
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
@@ -103,6 +102,7 @@ public class WeatherActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         if (weather != null && "ok".equals(weather.status)) {
+                            Log.d(TAG, "成功获取天气数据" + responseText);
                             SharedPreferences.Editor editor = PreferenceManager.
                                     getDefaultSharedPreferences(WeatherActivity.this)
                                     .edit();
@@ -112,6 +112,7 @@ public class WeatherActivity extends AppCompatActivity {
                         } else {
                             Toast.makeText(WeatherActivity.this, "服务器获取信息失败",
                                     Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, responseText);
                         }
                     }
                 });
@@ -148,15 +149,16 @@ public class WeatherActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         if (aqi != null && "ok".equals(aqi.getStatus())) {
+                            Log.d(TAG, "成功获取空气质量信息" + responseText);
                             SharedPreferences.Editor editor = PreferenceManager.
                                     getDefaultSharedPreferences(WeatherActivity.this)
                                     .edit();
                             editor.putString("aqi", responseText);
                             editor.apply();
-                            showAqiInfo(aqi);
                         } else {
                             Toast.makeText(WeatherActivity.this, "服务器获取信息失败",
                                     Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, responseText);
                         }
                     }
                 });
@@ -166,7 +168,7 @@ public class WeatherActivity extends AppCompatActivity {
 
     private void showWeatherInfo(Weather weather) {
         String cityName = weather.basic.getLocation();
-        String updateTime = weather.update.getLoc();
+        String updateTime = weather.update.getLoc().split(" ")[1];
         String degree = weather.now.getTmp() + "℃";
         String weatherInfo = weather.now.getCond_txt();
         titleCity.setText(cityName);
@@ -174,43 +176,56 @@ public class WeatherActivity extends AppCompatActivity {
         degreeText.setText(degree);
         weatherInfoText.setText(weatherInfo);
         forecastLayout.removeAllViews();
-        for (LifeStyle lifeStyle : weather.lifeStyleList) {
-            if (lifeStyle.getType().equals("comf")) {
-                String comfort = "舒适度：" + lifeStyle.getTxt();
-                comfortText.setText(comfort);
-            } else {
-                comfortText.setText("获取指数信息失败");
+        if (weather.lifeStyleList != null && weather.lifeStyleList.size() > 0) {
+            for (LifeStyle lifeStyle : weather.lifeStyleList) {
+                if (lifeStyle.getType().equals("comf")) {
+                    String comfort = "舒适度：" + lifeStyle.getTxt();
+                    comfortText.setText(comfort);
+                } else {
+                    comfortText.setText("获取指数信息失败");
+                }
+                if (lifeStyle.getType().equals("cw")) {
+                    String carWash = "洗车指数：" + lifeStyle.getTxt();
+                    carWashText.setText(carWash);
+                } else {
+                    carWashText.setText("获取指数信息失败");
+                }
+                if (lifeStyle.getType().equals("sport")) {
+                    String sport = "运动指数：" + lifeStyle.getTxt();
+                    sportText.setText(sport);
+                } else {
+                    sportText.setText("获取指数信息失败");
+                }
             }
-            if (lifeStyle.getType().equals("cw")) {
-                String carWash = "洗车指数：" + lifeStyle.getTxt();
-                carWashText.setText(carWash);
-            } else {
-                carWashText.setText("获取指数信息失败");
-            }
-            if (lifeStyle.getType().equals("sport")) {
-                String sport = "运动指数：" + lifeStyle.getTxt();
-                sportText.setText(sport);
-            } else {
-                sportText.setText("获取指数信息失败");
-            }
+        } else {
+            Log.d(TAG, "showWeatherInfo: 获取指数列表数据失败");
         }
-        for (DailyForecast dailyForecast : weather.dailyForecastList) {
-            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item,
-                    forecastLayout, false);
-            TextView dateText = view.findViewById(R.id.date_text);
-            TextView infoText = view.findViewById(R.id.info_text);
-            TextView maxText = view.findViewById(R.id.max_text);
-            TextView minText = view.findViewById(R.id.min_text);
-            dateText.setText(dailyForecast.getDate());
-            infoText.setText(dailyForecast.getCond_txt_d());
-            maxText.setText(dailyForecast.getTmp_max());
-            minText.setText(dailyForecast.getTmp_min());
-            forecastLayout.addView(view);
+        if (weather.dailyForecastList != null && weather.dailyForecastList.size() > 0) {
+            for (DailyForecast dailyForecast : weather.dailyForecastList) {
+                View view = LayoutInflater.from(this).inflate(R.layout.forecast_item,
+                        forecastLayout, false);
+                TextView dateText = view.findViewById(R.id.date_text);
+                TextView infoText = view.findViewById(R.id.info_text);
+                TextView maxText = view.findViewById(R.id.max_text);
+                TextView minText = view.findViewById(R.id.min_text);
+                dateText.setText(dailyForecast.getDate());
+                infoText.setText(dailyForecast.getCond_txt_d());
+                maxText.setText(dailyForecast.getTmp_max());
+                minText.setText(dailyForecast.getTmp_min());
+                forecastLayout.addView(view);
+            }
+        } else {
+            Log.d(TAG, "showWeatherInfo: 获取未来几天天气列表数据失败");
         }
+        weatherLayout.setVisibility(View.VISIBLE);
     }
 
     private void showAqiInfo(Aqi aqi) {
         if (aqi != null) {
+            View view=LayoutInflater.from(this).inflate(R.layout.aqi,
+                    aqiLayout,false);
+            TextView aqiText=view.findViewById(R.id.aqi_text);
+            TextView pm25Text=view.findViewById(R.id.pm25_text);
             aqiText.setText(aqi.getAir_now_city().getAqi());
             pm25Text.setText(aqi.getAir_now_city().getPm25());
         } else {
